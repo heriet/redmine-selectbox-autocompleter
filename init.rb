3139f7125c4e149bee81491afb65c0a63f4e3081
heriet <heriet@heriet.info>
2016-12-30 13:04:12 +0900

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :selectbox_autocompleter do
+  name 'Selectbox Autocompleter plugin'
+  author 'heriet'
+  description 'This plugin generate Autocomplete box for Select box'
+  version '1.0.0'
+  url 'https://github.com/heriet/redmine_selectbox_autocompleter'
+  author_url 'http://heriet.info'
+end
