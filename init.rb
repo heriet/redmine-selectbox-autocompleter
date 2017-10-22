4887b37fc096d8a42a85541bee99a57ce6040ccb
heriet <heriet@heriet.info>
2017-10-22 23:21:16 +0900

    fix version

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :selectbox_autocompleter do
   name 'Selectbox Autocompleter plugin'
   author 'heriet'
   description 'This plugin generate Autocomplete box for Select box'
-  version '1.1.2'
+  version '1.1.3'
   url 'https://github.com/heriet/redmine-selectbox-autocompleter'
   author_url 'http://heriet.info'
 
