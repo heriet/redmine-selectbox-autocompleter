ad064137d78d41b69a1224a423b32ff95012bd21
minoru.nagasawa <minoru.nagasawa@gmail.com>
2018-03-16 20:48:31 +0900

    fix to always scroll

--- assets/javascripts/selectbox_autocompleter.js
+++ assets/javascripts/selectbox_autocompleter.js
@@ -54,22 +54,24 @@ selectbox_autocompleter.generateSelectboxAutocompleter = function(id) {
     Array.prototype.forEach.call(options, function(option) {
       if(option.textContent.trim() == value.trim()) {
         if (isMultiple) {
+          var needsOnchange = false
           if (!selectbox[option.index].selected) {
-            // Chrome : Scroll to the item set to selectedIndex
-            // Edge   : Scroll to the item last set
-            var selectedOptions = new Array();
-            Array.prototype.forEach.call(options, function(opt) {
-              if (opt.selected) {
-                selectedOptions.push(opt.index);
-              }
-            });
-            selectbox.selectedIndex = option.index; // Chrome scrolls here 
-            Array.prototype.forEach.call(selectedOptions, function(selected) {
-              selectbox[selected].selected = true;
-            });
-            selectbox[option.index].selected = true; // Edge scrolls here
-            if (selectbox.onchange) { selectbox.onchange(); }
+            needsOnchange = true
           }
+          // Chrome : Scroll to the item set to selectedIndex
+          // Edge   : Scroll to the item last set
+          var selectedOptions = new Array();
+          Array.prototype.forEach.call(options, function(opt) {
+            if (opt.selected) {
+              selectedOptions.push(opt.index);
+            }
+          });
+          selectbox.selectedIndex = option.index; // Chrome scrolls here 
+          Array.prototype.forEach.call(selectedOptions, function(selected) {
+            selectbox[selected].selected = true;
+          });
+          selectbox[option.index].selected = true; // Edge scrolls here
+          if (needsOnchange && selectbox.onchange) { selectbox.onchange(); }
         } else {
           if(selectbox.selectedIndex != option.index) {
             selectbox.selectedIndex = option.index;
